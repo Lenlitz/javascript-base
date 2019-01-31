c77391965468bdd6d6df1a01694788e1d78683e9
Lena Loshakova <you@example.com>
2019-01-31 22:06:07 +0200

    tesk

--- lena_loshakova/test.js
+++ lena_loshakova/test.js
@@ -1,6 +1,30 @@
-var a = +prompt("Enter the first number :");
+ /*var a = +prompt("Enter the first number :");
 var b = +prompt("Enter the second number :");
 var functionBody = prompt("Enter the body :");
 
 var c = new Function( 'a, b', functionBody );
-c(a, b)
+//c(a, b)
+//document.write("result: " + functionBody + "</br>");
+function generateFunction() {
+var a = +prompt("Enter the first number :");
+var b = +prompt("Enter the second number :");
+var functionBody = prompt("Enter the body :");
+var c = new Function( 'a, b', functionBody );
+return c;
+}
+document.write("result: " + functionBody + "</br>");
+*/
+
+// var a = undefind;
+//console.log(a * 2);
+//var a = 5; //hoisting 
+
+function sumRange() {
+var a = arguments;
+var sum = 0;
+   for(var i = 0; i < arguments.length; i++){
+      sum += a[i];
+   }
+
+   return sum;
+}
