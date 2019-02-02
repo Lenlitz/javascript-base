592f7c660b5c701ab0da74d524a170ad307b79ff
Lena Loshakova <you@example.com>
2019-02-02 14:20:01 +0200

    ht

---
+++ lena_loshakova/class6.js
@@ -0,0 +1,55 @@
+var STATE = { // STATE object
+student: {}, // student object
+prepod: {}
+}
+runMyApp();
+
+
+function runMyApp(){
+    showMessage();
+    askNextCommand();
+}
+function showMessage(){
+   // document.write("Hello!");
+    var WelcomeText = `
+     <div class = "container"> 
+     <p>Hello</p>
+      </div> `
+    showAll( showStyle() + WelcomeText);
+}
+function showAll(text){
+    document.write(text);
+}
+
+function showStyle(){
+var STYLE = `
+<style>
+.container {
+padding: 10px;
+border: 1px;
+}
+</style>
+`;
+ return STYLE;
+}
+
+function askNextCommand(){
+    var command = prompt("Enter the command :");
+    if(command == 0){
+        showByeMessage();
+    } else {
+        askNextCommand();
+    }
+    }
+ function showByeMessage(){
+     var ByeText = `
+     <div class = "container"> 
+     <p>Bye</p>
+      </div> `
+ ClearScreen();
+ showAll(showStyle() + ByeText);
+ }
+
+ function ClearScreen(){
+     document.body.innerHTML ="";
+ }
